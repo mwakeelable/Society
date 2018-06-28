a2be7d7a97085b5640dec4cbe00f81106ebb90fa
unknown <mahmoud.wakeel.eg@gmail.com>
2018-06-28 17:09:34 +0200

    get user posts

--- app/src/main/java/com/wakeel/society/view/HomeActivity.java
+++ app/src/main/java/com/wakeel/society/view/HomeActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
@@ -25,10 +27,11 @@ import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
     private final static String TAG = HomeActivity.class.getSimpleName();
-    @BindView(R.id.homeTXT)
-    TextView homeTXT;
     String accessToken;
-    ArrayList<Post> posts = new ArrayList<>();
+    @BindView(R.id.postsList)
+    ListView postsList;
+    ArrayList<String> posts = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,8 +45,6 @@ public class HomeActivity extends AppCompatActivity {
                 HttpMethod.GET,
                 new GraphRequest.Callback() {
                     public void onCompleted(GraphResponse response) {
-//                        Log.d(TAG, response.getJSONObject().toString());
-                        homeTXT.setText(response.getJSONObject().toString());
                         try {
                             JSONObject postsParent = response.getJSONObject();
                             JSONArray postsArray = postsParent.getJSONArray("data");
@@ -54,12 +55,11 @@ public class HomeActivity extends AppCompatActivity {
                                     post.setCreated_time(postObj.optString("created_time"));
                                     post.setMessage(postObj.optString("message"));
                                     post.setId(postObj.optString("id"));
-                                    posts.add(post);
+                                    posts.add(post.getMessage());
                                 }
                             }
-                            for (int i = 0; i < posts.size(); i++) {
-                                Log.d(TAG, posts.get(i).getMessage());
-                            }
+                            adapter = new ArrayAdapter<String>(HomeActivity.this, android.R.layout.simple_list_item_1, posts);
+                            postsList.setAdapter(adapter);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
